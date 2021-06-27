b3d8989724294c6e48730544796333f00a74c9d5
songcs <songcs0329@gmail.com>
2021-06-27 15:13:44 +0900

    210627 간단한 폼 제작(vue-clli)

---
+++ vue-form/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input
+        type="text" 
+        id="username"
+        v-model="username"
+      />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input
+        type="password" 
+        id="password"
+        v-model="password"
+      />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    submitForm() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+      .then(res => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
