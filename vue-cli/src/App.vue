59187b874fef3d60e098bbd868da3810fbad011c
songcs <songcs0329@gmail.com>
2021-06-27 14:33:50 +0900

    210627 vue-cli 사용법 정리

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AppHeader
+      v-bind:propsData="str"
+      v-on:renew="renewStr"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
+  data() {
+    return {
+      str: 'Header',
+    };
+  },
   components: {
-    HelloWorld
+    AppHeader
+  },
+  methods: {
+    renewStr() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>{{ propsData }}</h1>
+    <button @click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["propsData"],
+  methods: {
+    sendEvent() {
+      this.$emit('renew');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
